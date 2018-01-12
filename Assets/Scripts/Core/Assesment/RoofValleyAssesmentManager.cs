ea054dc9a3ae33c971bbe69e7f5fe5e6f1a638bc
AkashGutha <akash.gutha@outlook.com>
2018-01-12 12:04:16 +0530

    added roof valley assesment

---
+++ Assets/Scripts/Core/Assesment/RoofValleyAssesmentManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoofValleyAssesmentManager : AssesmentManager {
+
+	private void Awake () {
+		this.assesment = new RoofValleyAssesmnent ();
+	}
+}
