7c274740bd1806fc71c49d94eb4d02c4fabf5636
AkashGutha <akash.gutha@outlook.com>
2018-01-12 12:07:24 +0530

    added ridge cap assesment

---
+++ Assets/Scripts/Core/Assesment/RidgeCapAssesmentManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RidgeCapAssesmentManager : AssesmentManager {
+
+	private void Awake() {
+		this.assesment = new RidgeCapAssesment();
+	}
+}
