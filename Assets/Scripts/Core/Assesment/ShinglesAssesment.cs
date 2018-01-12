9099dc0645a406f64697a03a7eb4289266aacee9
AkashGutha <akash.gutha@outlook.com>
2018-01-12 11:49:55 +0530

    added shingles assesment

---
+++ Assets/Scripts/Core/Assesment/ShinglesAssesment.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShinglesAssesment : Assesment {
+
+	public ShinglesAssesment()
+	{
+		this.IsComplete = false;
+	}
+}
---
+++ Assets/Scripts/Core/Assesment/ShinglesAssesmentManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShinglesAssesmentManager : AssesmentManager {
+
+	private void Awake () {
+		this.assesment = new ShinglesAssesment();
+	}
+}
